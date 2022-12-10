7c8bec5a4bf7295b9029364e322af878b9dcf6a9
Daniela Cubides <dani_6352@hotmail.com>
2022-12-10 06:12:04 -0600

    commit control

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
+import { MediaService } from '../core/services/media.service';
 
 @Component({
-  selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+    selector: 'app-tab3',
+    templateUrl: 'tab3.page.html',
+    styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
 
-  constructor() {}
+    constructor(private mediaService: MediaService) { }
 
 
-  newPhoto() {
-    
-  }
+    newPhoto() {
+        let x = this.mediaService.newUserMedia();
+    }
 
 }
