405216cd1070aad9d12d4db283cbd48b20127efa
Daniela Cubides <dani_6352@hotmail.com>
2022-12-11 08:28:11 -0500

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { FlexLayoutModule } from '@angular/flex-layout';
-
 import { AngularFireModule } from "@angular/fire/compat";
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { firebaseConfig } from './credentials';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FlexLayoutModule, AngularFireModule.initializeApp(firebaseConfig), AngularFirestoreModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(firebaseConfig), AngularFirestoreModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -5,15 +5,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 import { Tab2PageRoutingModule } from './tab2-routing.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    Tab2PageRoutingModule,
-    FlexLayoutModule
+    RouterModule.forChild([{ path: '', component: Tab2Page }]),
+    Tab2PageRoutingModule
   ],
   declarations: [Tab2Page]
 })
